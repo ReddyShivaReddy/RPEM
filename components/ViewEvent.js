a0315eb30144c34c0b1a55d2f91bdf378f18d536
ReddyShivaReddy <r.shivareddy2122@gmail.com>
2024-06-30 19:04:01 +0530

    commit

--- components/ViewEvent.js
+++ components/ViewEvent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StatusBar, Button, ScrollView, FlatList, Image, Dimensions, StyleSheet, TouchableOpacity, Modal,TextInput, } from 'react-native'
+import { View, Text, StatusBar, Button, ScrollView, FlatList, Image, Dimensions, StyleSheet, TouchableOpacity, Modal, TextInput, } from 'react-native'
 import RegisterForm from './RegisterForm'
 
 const { width } = Dimensions.get('window');
@@ -11,38 +11,70 @@ const ViewEvent = () => {
     const [please, setPlease] = useState(true)
     const [selectedItem2, setSelectedItem2] = useState({});
     const [modalVisible2, setModalVisible2] = useState(false);
-
+    const [Event, setEvent] = useState()
 
 
     const [Name, setName] = useState('');
     const [Email, setEmail] = useState('');
     const [Food, setFood] = useState('');
 
-    const handleNameChange=()=>{
-
+    const handleNameChange = (text) => {
+        setName(text);
     }
 
-    const handleFoodChange=()=>{
-        
+    const handleFoodChange = (text) => {
+        setFood(text)
     }
 
-    const handleEmailChange=()=>{
-        
+    const handleEmailChange = (text) => {
+        setEmail(text)
     }
+
+
 
     useEffect(() => {
         async function fetchData() {
-            const resData = await fetch("http://192.168.76.150:7777/api/View-Event");
+            const resData = await fetch("http://192.168.174.150:7777/api/View-Event");
             const data = await resData.json();
             setEventData(data);
             // console.log(data);
         }
         fetchData();
     }, [please]);
-    const registerEvent = ({ item }) => {
-        console.log("hello");
+
+
+    const registerData = {
+        "EmpName": Name,
+        "EmpEmail": Email,
+        "Food": Food,
+        "Event": Event
+    }
+
+    const register = async () => {
+        console.log(registerData);
+        const response = await fetch("http://192.168.174.150:7777/api/register", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(registerData),
+        })
+        console.log(response)
+        setModalVisible(false);
+        setModalVisible2(false);
+        setEmail();
+        setName();
+        setFood();
+    }
+
+
+
+    const registerEvent = ({ item = {selectedItem} }) => {
+        // console.log(item.selectedItem.title);
         setSelectedItem2(item);
         setModalVisible2(true);
+        setEvent(item.selectedItem.title)
     }
     const Renderitem = ({ item }) => (
         <TouchableOpacity onPress={() => {
@@ -92,7 +124,7 @@ const ViewEvent = () => {
                 onRequestClose={() => setModalVisible(false)}
             >
                 <ScrollView style={styles.modalContainer}>
-                    <Text style={{ fontSize: 30, padding: 15, color: '#8AAEE0', fontWeight: 'bold', textAlign: 'center' }}>Register Event</Text>
+                    {/* <Text style={{ fontSize: 30, padding: 15, color: '#8AAEE0', fontWeight: 'bold', textAlign: 'center' }}>Register Event</Text> */}
                     {/* {selectedItem && Object.keys(selectedItem).map((key) => (
                         <Text key={key} style={{ fontSize: 20, padding: 7, }}>
                             <Text style={{ fontWeight: 'bold', }}>{key}: </Text>{selectedItem[key]}
@@ -163,60 +195,57 @@ const ViewEvent = () => {
                 <ScrollView style={styles.modalContainer}>
                     <Text style={{ fontSize: 30, padding: 15, color: '#8AAEE0', fontWeight: 'bold', textAlign: 'center' }}>Register Event</Text>
 
-                    <View>
+                    <View style={{ paddingHorizontal: 10, borderRadius: 0.3 }}>
                         <Text>
                             Enter your name
                         </Text>
-                        <TextInput 
-                        value={Name}
-                        onChangeText={handleNameChange}
-                        style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10 }}
-                         />
-                         <Text>
+                        <TextInput
+                            value={Name}
+                            onChangeText={handleNameChange}
+                            style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, borderRadius: 10 }}
+                        />
+                        <Text>
                             Enter your email
                         </Text>
-                        <TextInput 
-                        value={Email}
-                        onChangeText={handleEmailChange}
-                        style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10 }}
-                         />
-                         <Text>
+                        <TextInput
+                            value={Email}
+                            onChangeText={handleEmailChange}
+                            style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, borderRadius: 10 }}
+                        />
+                        {selectedItem && Object.keys(selectedItem).map((key) => {
+                            
+                            return (
+                                <View>
+                                    {key === 'title' && (
+                                        <View>
+                                            <Text key={key} style={{ fontWeight: 'normal' }}>
+                                                {key === 'title'
+                                                    ? <Text>Event </Text>
+                                                    : key
+                                                }
+                                            </Text>
+                                            <TextInput
+                                                value={selectedItem[key]}
+                                                style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, borderRadius: 10 }}
+                                            />
+                                        </View>
+                                    )}
+                                </View>
+                            );
+                        })}
+                        <Text>
                             Veg / Non-Veg
                         </Text>
-                        <TextInput 
-                        value={Food}
-                        onChangeText={handleFoodChange}
-                        style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10 }}
-                         />
+                        <TextInput
+                            value={Food}
+                            onChangeText={handleFoodChange}
+                            style={{ height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, borderRadius: 10 }}
+                        />
                     </View>
 
-                    {selectedItem && Object.keys(selectedItem).map((key) => {
-                        return (
-                            <View style={{ paddingHorizontal: 10 }}>
-                                {key !== 'image' && (
-                                    <View>
-                                        <Text key={key} style={{ fontWeight: 'normal' }}>
-                                            {/* {key === 'title'
-                                                ? <Text style={{ fontWeight: 'bold' }}>Event: </Text>
-                                                : key === 'description'
-                                                    ? <Text style={{ fontWeight: 'bold' }}>About Event: </Text>
-                                                    : key === 'foodEvent'
-                                                        ? <Text style={{ fontWeight: 'bold' }}>Food: </Text>
-                                                        : key === 'date'
-                                                            ? '📅 '
-                                                            : key === 'location'
-                                                                ? '📍 ' : key
-                                            } */}
-                                            <Text>{selectedItem[key]}</Text>
-                                        </Text>
-                                    </View>
-                                )}
-                            </View>
-                        );
-                    })}
 
                     <View style={{ padding: 30, alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'row', gap: 20 }}>
-                        <Button title='Register' color='#717999' onPress={registerEvent} />
+                        <Button title='Register' color='#717999' onPress={register} />
                         <Button title='   Close   ' color='#717999' onPress={() => setModalVisible2(false)} />
                     </View>
                 </ScrollView>
